72cb928c3891b38781dc910705509a498e21dca2
Daniil Shmerko <Dan4ikshmerko@yandex.ru>
2025-04-30 12:31:03 +1000

    fixed some bugs

--- RSA-lab3.py
+++ RSA-lab3.py
@@ -1,99 +1,99 @@
-from math import gcd
+# Алфавит по Приложению 2
+alphabet = [
+    'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й',
+    'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
+    'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+    ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+]
 
-# Алфавит для шифрования
-ALPHABET = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-
-# Функция для получения индекса символа в алфавите
-def char_to_num(char):
-    return ALPHABET.index(char) + 1
-
-# Функция для получения символа по его индексу
-def num_to_char(num):
-    return ALPHABET[num - 1]
-
-# Функция для нахождения модульного обратного
-def mod_inverse(a, m):
-    for x in range(1, m):
-        if (a * x) % m == 1:
-            return x
-    return None
-def is_prime(num):
-    """Проверка числа на простоту."""
-    if num < 2:
+# Функция определения простоты
+def is_prime(n):
+    if n < 2:
         return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
     return True
 
+# НОД
 def gcd(a, b):
-    """Нахождение наибольшего общего делителя."""
     while b != 0:
         a, b = b, a % b
     return a
 
+# Обратный элемент по модулю
 def mod_inverse(e, phi):
-    """Нахождение обратного элемента d по модулю phi."""
-    k = 1
-    while (k * phi + 1) % e != 0:
-        k += 1
-    return (k * phi + 1) // e
+    for d in range(1, phi):
+        if (e * d) % phi == 1:
+            return d
+    return None
 
-# Класс для реализации RSA
-class RSA:
-    def __init__(self, p, q):
-        self.p = p
-        self.q = q
-        self.n = p * q
-        self.phi = (p - 1) * (q - 1)
-        self.e = self.find_e()
-        self.d = mod_inverse(self.e, self.phi)
+# Генерация ключей
+def generate_key_pair(p, q):
+    n = p * q
+    phi = (p - 1) * (q - 1)
 
-    def find_e(self):
-        """Нахождение открытой экспоненты e."""
-        for e in range(2, self.phi):
-            if gcd(e, self.phi) == 1:
-                return e
-        raise ValueError("Не удалось найти подходящее значение e.")
+    # Выбираем e как наибольшее простое < p
+    e = p - 1
+    while not is_prime(e) or gcd(e, phi) != 1:
+        e -= 1
 
-    def encrypt(self, message):
-        """Шифрование сообщения."""
-        encrypted = []
-        for char in message:
-            char_num = ord(char)  # Преобразуем символ в его ASCII-код
-            encrypted_char = pow(char_num, self.e, self.n)
-            encrypted.append(encrypted_char)
-        return encrypted
+    d = mod_inverse(e, phi)
+    return (e, n), (d, n)
 
-    def decrypt(self, encrypted_message):
-        """Дешифрование сообщения."""
-        decrypted = ""
-        for num in encrypted_message:
-            decrypted_char = pow(num, self.d, self.n)
-            decrypted += chr(decrypted_char)  # Преобразуем ASCII-код обратно в символ
-        return decrypted
+# Шифрование
+def encrypt(pub_k, msg):
+    e, n = pub_k
+    crypt = []
+    for char in msg:
+        try:
+            idx = alphabet.index(char.upper())
+        except ValueError:
+            print(f"Ошибка: Символ '{char}' отсутствует в алфавите.")
+            continue
+        encrypted = pow(idx, e, n)
+        crypt.append(encrypted)
+    return crypt
 
-# Основная часть программы
-if __name__ == "__main__":
-    # Исходные данные
-    p = 17
-    q = 41
-    message = "Шмерко"
-    cryptogram = [576, 142, 639, 421, 208, 608]
+# Дешифрование
+def decrypt(priv_k, cipher):
+    d, n = priv_k
+    decrypted = []
+    for num in cipher:
+        decoded_index = pow(num, d, n)
+        if 0 <= decoded_index < len(alphabet):
+            decrypted.append(alphabet[decoded_index])
+        else:
+            # Защита от выхода за границы алфавита
+            decoded_index %= len(alphabet)
+            decrypted.append(alphabet[decoded_index])
+    return ''.join(decrypted)
 
-    # Создание объекта RSA
-    rsa = RSA(p, q)
+# ================================
+#           Main
+# ================================
 
-    # Вывод ключей
-    print(f"Открытый ключ: (e, n) = ({rsa.e}, {rsa.n})")
-    print(f"Закрытый ключ: (d, n) = ({rsa.d}, {rsa.n})")
+# Простые числа для варианта 13
+p = 17
+q = 41
 
-    # Шифрование сообщения
-    encrypted_message = rsa.encrypt(message)
-    print(f"Исходное сообщение: {message}")
-    print(f"Зашифрованное сообщение: {encrypted_message}")
+# Генерация ключей
+public, private = generate_key_pair(p, q)
+print("Публичный ключ: ", public)
+print("Приватный ключ: ", private)
 
-    # Дешифрование криптограммы
-    decrypted_message = rsa.decrypt(cryptogram)
-    print(f"Криптограмма: {cryptogram}")
-    print(f"Расшифрованное сообщение: {decrypted_message}")
+# Исходное сообщение
+message = "ШМЕРКО"
+encrypted_msg = encrypt(public, message)
+print("\nИсходное сообщение:", message)
+print("Зашифрованное сообщение:", encrypted_msg)
+
+# Расшифрование
+decrypted_msg = decrypt(private, encrypted_msg)
+print("Расшифрованное сообщение:", decrypted_msg)
+
+# Расшифровка криптограммы
+cryptogram = [576, 142, 639, 421, 208, 608]
+decrypted_cryptogram = decrypt(private, cryptogram)
+print("\nКриптограмма:", cryptogram)
+print("Расшифрованное сообщение (криптограммы):", decrypted_cryptogram)
